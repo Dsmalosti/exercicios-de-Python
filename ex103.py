b1fd9af97f66ece22c37c5441e94c806c3f8a3fc
Diogo Santana Malosti <dsmalosti@gmail.com>
2025-01-03 10:33:34 -0300

    estudo de funções

---
+++ ex103.py
@@ -0,0 +1,16 @@
+def ficha(nome='<desconhecido>', gol=0):
+    print(f'O {nome} fez {gol} gol(s) no campeonato')
+
+
+n = str(input('Nome do jogador? '))
+g = str(input('Quantos gols: '))
+
+if g.isnumeric():
+    g = int(g)
+else:
+    g = 0
+if n.strip() == '':
+    ficha(gol=g)
+else:
+    ficha(n, g)
+
